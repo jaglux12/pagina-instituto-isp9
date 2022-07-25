31c13e6cccf77c2f9f114d6f176e5f276fa0f4f2
jaglux12 <gonzalopez239@gmail.com>
2022-07-25 12:11:34 -0300

    agregamos las carreras para mobile

--- references/articulo_carreras.php
+++ references/articulo_carreras.php
@@ -1,13 +1,49 @@
-<div class="container-fluid d-flex justify-content-center">
+<div class="container-fluid d-flex justify-content-center px-4 py-2">
 
-  <div class="row">
-    <div class="col-md-6 py-4 d-flex justify-content-center align-items-center flex-column">
-          <img src="../img/personas/tec_superior_desarrollo.jpeg" alt="" width="50%" height="50%">
-          <h1>fsdfs</h1>
+  <div class="row d-md-none">
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/tec_superior_desarrollo.jpeg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN DESARROLLO DE SOFTWARE</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
-    <div class="col-md-6">
-      <h1>fsdfsd</h1>
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/tec_organizaciones.jpg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN GESTIÓN DE LAS ORGANIZACIONES</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/tec_enfermera.jpg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">TÉCNICO SUPERIOR <br>EN ENFERMERÍA</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
+    </div>
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/tec_rural.jpg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">TÉCNICO SUPERIOR <br>EN ADMINISTRACIÓN RURAL</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
+    </div>
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/profesorado_primaria.jpg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">PROFESORADO <br>DE EDUCACIÓN PRIMARIA</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
+    </div>
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/profesorado_especial.jpg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">PROFESORADO <br>DE EDUCACIÓN ESPECIAL</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
+    </div>
+
+    <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
+          style="background-image: url(img/personas/profesorado_inicial.jpg);" id="panelCarrera">
+          <h1 id="tituloCarrera" class="text-center">PROFESORADO <br>DE EDUCACIÓN INICIAL</h1>
+          <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
+    </div>
+    
   </div>
 
 </div>
