22460fc09c0f837b407a6eba509659086c116d08
jaglux12 <gonzalopez239@gmail.com>
2022-07-25 10:59:39 -0300

    agregamos nuevas imagenes

--- references/articulo_carreras.php
+++ references/articulo_carreras.php
@@ -1,8 +1,9 @@
 <div class="container-fluid d-flex justify-content-center">
 
   <div class="row">
-    <div class="col-md-6">
-      <h1>fsdfs</h1>
+    <div class="col-md-6 py-4 d-flex justify-content-center align-items-center flex-column">
+          <img src="../img/personas/tec_superior_desarrollo.jpeg" alt="" width="50%" height="50%">
+          <h1>fsdfs</h1>
     </div>
     <div class="col-md-6">
       <h1>fsdfsd</h1>
