5d234bc9ce4df4b46fcdbe5f87d2f71abd4eb35d
jaglux12 <gonzalopez239@gmail.com>
2022-08-04 17:24:08 -0300

    cambiamos el nombre de la carpeta personas a estudiantes

--- references/articulo_carreras.php
+++ references/articulo_carreras.php
@@ -3,43 +3,43 @@
   <div class="row d-md-none">
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/tec_superior_desarrollo.jpeg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/tec_superior_desarrollo.jpeg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN DESARROLLO DE SOFTWARE</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/tec_organizaciones.jpg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/tec_organizaciones.jpg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN GESTIÓN DE LAS ORGANIZACIONES</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/tec_enfermera.jpg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/tec_enfermera.jpg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">TÉCNICO SUPERIOR <br>EN ENFERMERÍA</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/tec_rural.jpg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/tec_rural.jpg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">TÉCNICO SUPERIOR <br>EN ADMINISTRACIÓN RURAL</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/profesorado_primaria.jpg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/profesorado_primaria.jpg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">PROFESORADO <br>DE EDUCACIÓN PRIMARIA</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/profesorado_especial.jpg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/profesorado_especial.jpg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">PROFESORADO <br>DE EDUCACIÓN ESPECIAL</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
 
     <div class="col-xs-1 d-flex justify-content-center p-5 align-items-center my-2 flex-column rounded"
-          style="background-image: url(img/personas/profesorado_inicial.jpg);" id="panelCarrera">
+          style="background-image: url(img/estudiantes/profesorado_inicial.jpg);" id="panelCarrera">
           <h1 id="tituloCarrera" class="text-center">PROFESORADO <br>DE EDUCACIÓN INICIAL</h1>
           <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
     </div>
@@ -64,13 +64,13 @@
   <div class="row d-none d-md-block">
     <div class="row px-4 d-flex justify-content-center">
       <div class="col-md-5 d-flex justify-content-center me-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/tec_superior_desarrollo.jpeg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/tec_superior_desarrollo.jpeg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN DESARROLLO DE SOFTWARE</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
 
       <div class="col-md-5 d-flex justify-content-center ms-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/tec_organizaciones.jpg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/tec_organizaciones.jpg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN GESTIÓN DE LAS ORGANIZACIONES</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
@@ -78,13 +78,13 @@
 
     <div class="row px-4 d-flex justify-content-center">
       <div class="col-md-5 d-flex justify-content-center me-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/tec_enfermera.jpg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/tec_enfermera.jpg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN ENFERMERÍA</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
 
       <div class="col-md-5 d-flex justify-content-center ms-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/tec_rural.jpg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/tec_rural.jpg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">TECNICO SUPERIOR <br>EN ADMINISTRACIÓN RURAL</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
@@ -92,13 +92,13 @@
 
     <div class="row px-4 d-flex justify-content-center">
       <div class="col-md-5 d-flex justify-content-center me-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/profesorado_primaria.jpg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/profesorado_primaria.jpg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">PROFESORADO DE  <br>EDUCACIÓN PRIMARIA</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
 
       <div class="col-md-5 d-flex justify-content-center ms-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/profesorado_especial.jpg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/profesorado_especial.jpg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">PROFESORADO DE <br> EDUCACIÓN ESPECIAL</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
@@ -106,7 +106,7 @@
 
     <div class="row px-4 d-flex justify-content-center">
       <div class="col-md-5 d-flex justify-content-center me-2 p-5 align-items-center my-2 flex-column rounded"
-            style="background-image: url(img/personas/profesorado_primaria.jpg);" id="panelCarrera">
+            style="background-image: url(img/estudiantes/profesorado_primaria.jpg);" id="panelCarrera">
             <h1 id="tituloCarrera" class="text-center">PROFESORADO DE  <br>EDUCACIÓN INICIAL</h1>
             <button type="button" class="btn btn-outline-light mt-4">SABER MÁS</button>
       </div>
